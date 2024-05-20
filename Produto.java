a1c084ccb1b7145fadee5bdc1f240e329627e5f8
Vitor Lins <107719052+vbreno@users.noreply.github.com>
2024-05-20 20:12:48 -0300

    Add files via upload
    Adição do "banco de dados" de cliente e produto e da classe de produtos.

---
+++ Produto.java
@@ -0,0 +1,51 @@
+public class Produto {
+    private String nome;
+    private String codigo;
+    private double preco;
+
+    public Produto(String nome, String codigo, double preco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return nome + "," + codigo + "," + preco;
+    }
+
+    public static Produto fromString(String linha) {
+        String[] partes = linha.split(",");
+        if (partes.length != 3) {
+            throw new IllegalArgumentException("Formato da linha inválido: " + linha);
+        }
+        String nome = partes[0];
+        String codigo = partes[1];
+        double preco = Double.parseDouble(partes[2]);
+        return new Produto(nome, codigo, preco);
+    }
+}
